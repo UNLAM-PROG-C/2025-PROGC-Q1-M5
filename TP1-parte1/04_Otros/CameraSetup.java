0d120f783c35cf3a9daf55862a8da55bae026244
LEO <89648679+LeonardoDBarbaro@users.noreply.github.com>
2025-05-14 23:05:56 -0300

    Update CameraSetup.java

--- TP1-parte1/04_Otros/CameraSetup.java
+++ TP1-parte1/04_Otros/CameraSetup.java
@@ -3,8 +3,10 @@ import java.io.File;
 import java.util.ArrayList;
 
 
-public class CameraSetup {
-    public ArrayList<String> SetupCameras(String[] zones, Integer quantity) {
+public class CameraSetup
+{
+    public ArrayList<String> SetupCameras(String[] zones, Integer quantity)
+    {
 
         ArrayList<String> subFoldersPaths = new ArrayList<>();
 
@@ -12,7 +14,8 @@ public class CameraSetup {
         File rootFolder = new File(rootFolderName);
         rootFolder.mkdir();
     
-        for (int i = 1; i <= quantity; i++) {
+        for (int i = 1; i <= quantity; i++)
+        {
             String subFolderName = zones[i];
             File subFolder = new File(rootFolder, subFolderName);
             subFolder.mkdir();
