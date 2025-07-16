997c3ef38146ded0ef31deb590e7710e8c5e1d9e
LeonardoDBarbaro <Leonbarbaro@hotmail.com>
2025-07-16 12:37:33 -0300

    Cambio de path

--- TP2-parte3/04_Otros/Scenes/gestorNombreEquipo.gd
+++
@@ -1,5 +0,0 @@
-extends VBoxContainer
-
-func _on_scroll_colores_equipo_cambiado(equipo_node: Variant, posicion: Variant) -> void:
-	get_node("Nombre").set_text(equipo_node.nombre)
-	get_node("Abreviacion").set_text(equipo_node.abreviacion)
--- TP2-parte3/04_Otros/Scripts/animation_playerColors.gd
+++
@@ -1,7 +0,0 @@
-extends AnimationPlayer
-
-func _on_color_boton_pressed():
-	play("SlideOutColores")
-
-func _on_color_boton_2_pressed():
-	play("SlideOutColores")
